a2b60459f9ee726549f4b2e6b93bdd577ff4d57d
Gnaixuj <gnaixujuxiang@gmail.com>
2020-07-13 16:37:59 +0800

    2nd Commit - searchItem Route Updated

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -6,25 +6,56 @@ const search = (req, res) => {
 }
 
 const searchItem = async (req, res) => {
-    let apiurl;
-    const query = '&query=' + req.body.query;
-    if (req.body.searchBy == 'byMovie') {
-        apiurl = tmdb.searchMovies + query;
-    }
-    else if (req.body.searchBy == 'byPerson') {
-        apiurl = tmdb.searchPerson + query;
-    }
-    const options = {
-        url: apiurl,
+    let apiurl, results;
+	const baseimgurl = tmdb.baseimgurl('w500');
+    var options = {
         method: 'GET'
     };
     try {
-        const results = await axios(options);
-        // console.log(results.data.results[0].known_for);
-        const baseimgurl = tmdb.baseimgurl('w500');
-        res.render('searchResultView', { query: req.body.query, data: results.data, baseimgurl });
-    }
-    catch (err) {
+        const query = '&query=' + req.body.query;
+        if (req.body.searchBy == 'byMovie') {
+            apiurl = tmdb.searchMovies + query;
+            options.url = apiurl;
+            results = await axios(options);
+			res.render('searchResultView', { query: req.body.query, data: results.data, baseimgurl });
+        }
+        else if (req.body.searchBy == 'byPerson') {
+            apiurl = tmdb.searchPerson + query;
+            options.url = apiurl;
+            const persons = await axios(options);
+            var personsInfo = [];
+            persons.data.results.forEach(person => {
+                var personInfo = {};
+                personInfo.known_for = person.known_for_department;
+                personInfo.uri = tmdb.getMovieCredits(person.id)
+                personsInfo.push(personInfo);
+            });
+    
+            var data = {
+                results: [],
+                total_results: 0
+            };
+            const promises = personsInfo.map(async personInfo => {
+                options.url = personInfo.uri;
+                const results = axios(options);
+                return results;
+            });
+            const results = await Promise.all(promises);
+
+            for (let i = 0; i < results.length; i++) {
+                if (personsInfo[i].known_for == "Acting") {
+                    data.total_results = data.total_results + results[i].data.cast.length;
+                    data.results.push(...results[i].data.cast);
+                }
+                else {
+                    data.total_results = data.total_results + results[i].data.crew.length;
+                    data.results.push(...results[i].data.crew);
+                }
+            }
+            // console.log(data);
+            res.render('searchResultView', { query: req.body.query, data, baseimgurl });
+        }
+    } catch (err) {
         console.error(err);
     }
 }
